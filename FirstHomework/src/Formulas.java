561f1d296f630686e0b2fdc3d2584a0e8026de2d
Natalia1910 <nkozak4818@gmail.com>
2019-09-29 20:19:08 +0300

    First

---
+++ FirstHomework/src/Formulas.java
@@ -0,0 +1,14 @@
+public class Formulas {
+    public static void main(String[] args) {
+        var a = -2.6;
+        var b = 0;
+        var c = 10;
+        var d = -6;
+        var e = 1.5;
+        System.out.println((a + b) / (d - b) * 2);
+        System.out.println(a + 2 * 2 - 12 / a * (b - c));
+        System.out.println(b + 11 * c - a);
+        System.out.println(a*(c + b + d) - e / 1 + a);
+        System.out.println(a + b *(a - b) / 12);
+    }
+}
---
+++ MyFirstProject/src/First.java
@@ -0,0 +1,11 @@
+public class First {
+    public static void main(String[] args) {
+        String N = "Name: ";
+        String Name = "Natalia Kozak";
+        String G = "Group: ";
+        String Group = "CAm-11";
+        String A = "Age: ";
+        String Age = "20";
+        System.out.print(N + Name + "\n" + G + Group + "\n" + A + Age);
+    }
+}
